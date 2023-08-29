0fe1b50a0d2239e3d365ede244a818400a94ceb2
MIV20 <mmphail@gmail.com>
2023-08-29 09:39:05 -0700

    day_5_final_and_updates

---
+++ day5_fizz_buzz.py
@@ -0,0 +1,33 @@
+def fizz_buzz(number: int) -> str:
+    """
+    Play Fizz buzz
+
+    :param number: The number to check.
+    :return: 'fizz' if the number is divisible by 3.
+        'buzz' if it's divisible by 5.
+        'fizz buzz' if it's divisible by both 3 and 5.
+        The number, as a string, otherwise.
+    """
+    if number % 15 == 0:
+        return "fizz buzz"
+    elif number % 3 == 0:
+        return "fizz"
+    elif number % 5 == 0:
+        return "buzz"
+    else:
+        return str(number)
+
+for i in range(1, 101):
+    print(fizz_buzz(i))
+
+
+
+for number in range(1, 101):
+    if number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    elif number % 5 == 0:
+        print("Buzz")
+    else:
+        print(number)
